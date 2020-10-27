64e8c8a43dad77d3fbf5479badac2f72c4269dc3
MCFrank16 <boonatechnologies@gmail.com>
2020-10-27 10:48:38 +0200

    adding users routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles # you can use this method to specify whay you only want to use
   #, only: [:show, :index, :create, :new, :edit, :update]
+  get 'signup', to: 'users#new'
+  resources :users, expect: [:new]
 end
