cc728efa5922977d3d5ecbf243c993b695b96195
MCFrank16 <boonatechnologies@gmail.com>
2020-11-09 16:27:04 +0200

    Restrict actions in controller level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Alpha blog, you are signed up as #{@user.username}"
             redirect_to articles_path
         else
